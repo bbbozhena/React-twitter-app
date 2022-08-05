a475c9aad88deb2099251557cae3305985fe5737
bbbozhena <bbbozhenka@gmail.com>
2022-08-05 14:46:04 +0300

    first commit

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>bbbozhena</h1>
+            <h2>5 posts, like 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SeachPanel = () => {
+  return (
+    <input
+      className="form-control search-input"
+      type="text"
+      placeholder="Search"
+    />
+  );
+};
+export default SeachPanel;
