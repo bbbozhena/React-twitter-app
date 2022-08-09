2c5ad134bf9acaa989a444b0f0e79eafb9372a47
bbbozhena <bbbozhenka@gmail.com>
2022-08-09 14:58:42 +0300

    mod props item

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const PostListItem = () => {
+const PostListItem = ({label}) => {
   return (
     <li className="app-list-item d-flex justify-content-between">
-      <span className="app-list-item-label">Hello World!</span>
+      <span className="app-list-item-label">{label}</span>
       <div className="d-flex justify-content-center aligh-items-center">
         <button type="button" className="btn-star btn-sm">
           <i className="fa fa-star"></i>
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -5,9 +5,9 @@ import  PostListItem from "../post-list-item"
 const PostList = () => {
     return (
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            <PostListItem label="Going to learn React"/>
+            <PostListItem label="That is so cool"/>
+            <PostListItem label="I need a break.."/>
         </ul>
     )
 }
