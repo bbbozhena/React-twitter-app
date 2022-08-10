eb60153b9b96ed050d1b03104c643b4c780fb6a4
bbbozhena <bbbozhenka@gmail.com>
2022-08-10 23:14:29 +0300

    add reactstrap and cssinjs

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,12 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+
+const Header = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  h1 {
+    font-size: 26px;
+  }
+  h2 {
+    font-size: 1.2rem;
+    color: grey;
+  }
+`;
 
 const AppHeader = () => {
-    return (
-        <div className="app-header d-flex">
-            <h1>bbbozhena</h1>
-            <h2>5 posts, like 0</h2>
-        </div>
-    )
-}
+  return (
+    <Header>
+      <h1>bbbozhena</h1>
+      <h2>5 posts, like 0</h2>
+    </Header>
+  );
+};
 
 export default AppHeader;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,39 +1,43 @@
 import React from "react";
-import 'font-awesome/css/font-awesome.min.css';
+import "font-awesome/css/font-awesome.min.css";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
-import PostStatusFilter from "../post-status-filter"
+import PostStatusFilter from "../post-status-filter";
 import PostList from "../post-list";
 import PostAddForms from "../post-add-form";
 
-import "./app.css"
-import "../app-header/app-header.css"
-import "../post-add-form/post-add-form.css"
-import "../post-list/post-list.css"
-import "../post-list-item/post-list-item.css"
-import "../post-status-filter/post-status-filter.css"
-import "../search-panel/search-panel.css"
+import "./app.css";
+import "../app-header/app-header.css";
+import "../post-add-form/post-add-form.css";
+import "../post-list/post-list.css";
+import "../post-list-item/post-list-item.css";
+import "../post-status-filter/post-status-filter.css";
+import "../search-panel/search-panel.css";
 
+import styled from "styled-components";
+
+const AppBlock = styled.div `
+  margin: 0 auto;
+  max-width: 800px;
+`;
 
 const App = () => {
-
   const data = [
-    {label: "Going to learn React",  important: true, id: "hdl"},
-    {label: "That is so cool",  important: false, id: "kdkd"},
-    {label: "I need a break..",  important: false, id: "dnl"}
+    { label: "Going to learn React", important: true, id: "hdl" },
+    { label: "That is so cool", important: false, id: "kdkd" },
+    { label: "I need a break..", important: false, id: "dnl" },
   ];
 
-
   return (
-    <div className="app">
+    <AppBlock>
       <AppHeader />
       <div className="search-panel d-flex">
         <SearchPanel />
-        <PostStatusFilter/>
+        <PostStatusFilter />
       </div>
-      <PostList posts = {data}/>
-      <PostAddForms/>
-    </div>
+      <PostList posts={data} />
+      <PostAddForms />
+    </AppBlock>
   );
 };
 
