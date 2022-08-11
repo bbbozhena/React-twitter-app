0ab5523a4e46519b7b5c51a25ddcbc98d5e0f465
bbbozhena <bbbozhenka@gmail.com>
2022-08-11 15:02:52 +0300

    add delete items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import "font-awesome/css/font-awesome.min.css";
 import AppHeader from "../app-header";
 import SearchPanel from "../search-panel";
@@ -16,29 +16,52 @@ import "../search-panel/search-panel.css";
 
 import styled from "styled-components";
 
-const AppBlock = styled.div `
+const AppBlock = styled.div`
   margin: 0 auto;
   max-width: 800px;
 `;
 
-const App = () => {
-  const data = [
-    { label: "Going to learn React", important: true, id: "hdl" },
-    { label: "That is so cool", important: false, id: "kdkd" },
-    { label: "I need a break..", important: false, id: "dnl" },
-  ];
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { label: "Going to learn React", important: true, id: "hdl" },
+        { label: "That is so cool", important: false, id: "kdkd" },
+        { label: "I need a break..", important: false, id: "dnl" },
+      ],
+    };
+    this.deleteItem = this.deleteItem.bind(this);
+  }
 
-  return (
-    <AppBlock>
-      <AppHeader />
-      <div className="search-panel d-flex">
-        <SearchPanel />
-        <PostStatusFilter />
-      </div>
-      <PostList posts={data} />
-      <PostAddForms />
-    </AppBlock>
-  );
-};
+  deleteItem (id) {
+    this.setState(({data}) => {
+      const index = data.findIndex(elem => elem.id === id);
 
-export default App;
+      const before = data.slice(0, index);
+      const after = data.slice (index + 1);
+
+      const newArr = [...before, ...after];
+
+      return {
+        data: newArr
+      }
+    });
+  }
+
+  render() {
+    return (
+      <AppBlock>
+        <AppHeader />
+        <div className="search-panel d-flex">
+          <SearchPanel />
+          <PostStatusFilter />
+        </div>
+        <PostList 
+        posts={this.state.data} 
+        onDelete={this.deleteItem} />
+        <PostAddForms />
+      </AppBlock>
+    );
+  }
+}
