04d7cf3acc9997f89e3230a52759658f0d246db9
bbbozhena <bbbozhenka@gmail.com>
2022-08-14 15:10:30 +0300

    add search

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -4,7 +4,6 @@ import { Button } from "reactstrap";
 const PostStatusFilter = () => {
     return (
         <div className="btn-group">
-            <Button outline color="info">All</Button>
             <button type="button" className="bt btn-info">All</button>
             <button type="button" className="btn btn-outline-secondary">Liked</button>
         </div>
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SeachPanel = () => {
-  return (
-    <input
-      className="form-control search-input"
-      type="text"
-      placeholder="Search"
-    />
-  );
-};
-export default SeachPanel;
+export default class SeachPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+    };
+    this.onUpdateSearch = this.onUpdateSearch.bind(this);
+  }
+  onUpdateSearch(e) {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onUpdateSearch(term);
+  }
+  render() {
+    return (
+      <input
+        className="form-control search-input"
+        type="text"
+        placeholder="Search"
+        onChange={this.onUpdateSearch}
+      />
+    );
+  }
+}
